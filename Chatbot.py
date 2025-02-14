8e94d095e8a3ee401ef4c29ad6b296c17dc4d5ad
fuzzypandafat <83717342+fuzzypandafat@users.noreply.github.com>
2025-02-14 12:27:40 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+from openai import AzureOpenAI
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version="2024-02-01",
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+    azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    )
+
+with st.sidebar:
+    "[Developed for Gen AI masterclass training](https://tcs.com/)"
+
+st.title("💬 Chatbot")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+    
+if prompt := st.chat_input():
+    
+
+    # client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
